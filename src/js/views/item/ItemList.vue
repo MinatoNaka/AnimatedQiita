2d44c13391566313d6ac92aec38e201b4428d255
aq_minato.naka <minato.naka@asia-quest.jp>
2019-12-25 17:09:54 +0900

    記事一覧取得処理実装

--- src/js/views/item/ItemList.vue
+++ src/js/views/item/ItemList.vue
@@ -1,13 +1,36 @@
 <template>
     <div>
         <p>Item List</p>
-
-        <router-link v-bind:to="{name: 'item.detail', params: {itemId: 1}}">
-            <button class="btn btn-success">Item Detail</button>
-        </router-link>
+        <ul>
+            <li v-for="item in items">
+                <router-link v-bind:to="{name: 'item.detail', params: {itemId: item.id}}">
+                    <button class="btn btn-success">Item Detail</button>
+                </router-link>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+        data: function () {
+            return {
+                items: []
+            }
+        },
+        mounted() {
+            this.getItems();
+        },
+        methods: {
+            getItems() {
+                axios.get('https://qiita.com/api/v2/items?page=1&per_page=20', {
+                    headers: {
+                        "Authorization": "Bearer " + process.env.MIX_QIITA_API_TOKEN
+                    }
+                }).then((res) => {
+                    this.items = res.data;
+                });
+            }
+        }
+    }
 </script>
