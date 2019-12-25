db2784fa9020e3f792f92e0bd41ddeb331e03242
aq_minato.naka <minato.naka@asia-quest.jp>
2019-12-25 17:28:27 +0900

    タグ一覧得処理実装

--- src/js/views/tag/TagList.vue
+++ src/js/views/tag/TagList.vue
@@ -1,13 +1,36 @@
 <template>
     <div>
         <p>Tag List</p>
-
-        <router-link v-bind:to="{name: 'tag.detail', params: {tagId: 1}}">
-            <button class="btn btn-success">Tag Detail</button>
-        </router-link>
+        <ul>
+            <li v-for="tag in tags">
+                <router-link v-bind:to="{name: 'tag.detail', params: {tagId: tag.id}}">
+                    <button class="btn btn-success">Tag Detail</button>
+                </router-link>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+        data: function () {
+            return {
+                tags: []
+            }
+        },
+        mounted() {
+            this.getTags();
+        },
+        methods: {
+            getTags() {
+                axios.get('https://qiita.com/api/v2/tags?page=1&per_page=20', {
+                    headers: {
+                        "Authorization": "Bearer " + process.env.MIX_QIITA_API_TOKEN
+                    }
+                }).then((res) => {
+                    this.tags = res.data;
+                });
+            }
+        }
+    }
 </script>
