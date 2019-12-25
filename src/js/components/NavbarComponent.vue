44e3a919d183f38f82cbb69ce6d078ae877e935e
aq_minato.naka <minato.naka@asia-quest.jp>
2019-12-25 22:30:02 +0900

    ヘッダーナビのスタイル修正

--- src/js/components/NavbarComponent.vue
+++ src/js/components/NavbarComponent.vue
@@ -1,26 +1,26 @@
 <template>
-    <div class="container-fluid bg-primary">
+    <div class="container-fluid bg-primary-dark">
         <div class="container">
-            <nav class="navbar navbar-expand-sm navbar-light">
-                <a class="navbar-brand" href="#">Example & Example</a>
+            <nav class="navbar navbar-expand-sm navbar-dark">
+                <a class="navbar-brand hvr-bounce-in" href="#">Animated Qiita</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ml-auto small">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Menu１<span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Menu２</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Menu３</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Menu４</a>
-                        </li>
+                    <ul class="navbar-nav ml-auto">
+                        <router-link tag="li" v-bind:to="{name: 'item.list'}" class="nav-item hvr-underline-from-left"
+                                     exact-active-class="active">
+                            <a class="nav-link">Articles</a>
+                        </router-link>
+                        <router-link tag="li" v-bind:to="{name: 'user.list'}" class="nav-item hvr-underline-from-left"
+                                     exact-active-class="active">
+                            <a class="nav-link">Users</a>
+                        </router-link>
+                        <router-link tag="li" v-bind:to="{name: 'tag.list'}" class="nav-item hvr-underline-from-left"
+                                     exact-active-class="active">
+                            <a class="nav-link">Tags</a>
+                        </router-link>
                     </ul>
                 </div>
             </nav>
