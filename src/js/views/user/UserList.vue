f62682967ae697ec277e0a36e3905d5d78fc9ee4
aq_minato.naka <minato.naka@asia-quest.jp>
2019-12-25 17:20:35 +0900

    ユーザ一覧取得処理実装

--- src/js/views/user/UserList.vue
+++ src/js/views/user/UserList.vue
@@ -1,13 +1,36 @@
 <template>
     <div>
         <p>User List</p>
-
-        <router-link v-bind:to="{name: 'user.detail', params: {userId: 1}}">
-            <button class="btn btn-success">User Detail</button>
-        </router-link>
+        <ul>
+            <li v-for="user in users">
+                <router-link v-bind:to="{name: 'user.detail', params: {userId: user.id}}">
+                    <button class="btn btn-success">User Detail</button>
+                </router-link>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+        data: function () {
+            return {
+                users: []
+            }
+        },
+        mounted() {
+            this.getUsers();
+        },
+        methods: {
+            getUsers() {
+                axios.get('https://qiita.com/api/v2/users?page=1&per_page=20', {
+                    headers: {
+                        "Authorization": "Bearer " + process.env.MIX_QIITA_API_TOKEN
+                    }
+                }).then((res) => {
+                    this.users = res.data;
+                });
+            }
+        }
+    }
 </script>
